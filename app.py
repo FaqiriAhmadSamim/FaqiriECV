ba6d4fe49256599cdaa69ad615b8cef7283bc89b
FAQIRI AHMAD SAMIM <145334940+FaqiriAhmadSamim@users.noreply.github.com>
2024-01-26 16:21:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,14 +25,6 @@ SOCIAL_MEDIA = {
     "GitHub": "https://github.com/FaqiriAhmadSamim",
     "Twitter": "https://twitter.com/Samimkhan11",
 }
-PROJECTS = {
-    "🏆 Sales Dashboard - Comparing sales across three stores": "https://youtu.be/Sb0A9i6d320",
-    "🏆 Income and Expense Tracker - Web app with NoSQL database": "https://youtu.be/3egaMfE9388",
-    "🏆 Desktop Application - Excel2CSV converter with user settings & menubar": "https://youtu.be/LzCfNanQ_9c",
-    "🏆 MyToolBelt - Custom MS Excel add-in to combine Python & Excel": "https://pythonandvba.com/mytoolbelt/",
-}
-
-
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
 
